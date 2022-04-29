c35ee94b9adf6d1743f96da257037d4179a2c830
Joseph Barbone <JoeBarbone@aol.com>
2022-04-29 16:51:40 -0400

    added highscore feature

--- js/game.js
+++ js/game.js
@@ -146,12 +146,31 @@ var startGame = function() {
 
 // Function to end the entire game
 var endGame = function() {
-    // If player is still alive, player wins!
-    if (playerInfo.health > 0) {
-        window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
-    } else {
-        window.alert("You've lost your robot in battle.");
+
+    window.alert("The game has ended. Let's see how you did!");
+
+    // Checking local storage for high score, if it's not there, use 0
+    var highScore = localStorage.getItem("highscore");
+    if (highScore === null) {
+        highScore = 0;
     }
+
+    // if player has more money than the high score, player has new high score
+    if (playerInfo.money > highScore) {
+        localStorage.setItem("highscore", playerInfo.money);
+        localStorage.setItem("name", playerInfo.name);
+
+        alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+    } else {
+        alert(playerInfo.name + " you did not beat the high score of " + highScore + ". Maybe next time!");
+    }
+
+    // // If player is still alive, player wins!
+    // if (playerInfo.health > 0) {
+    //     window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
+    // } else {
+    //     window.alert("You've lost your robot in battle.");
+    // }
 
     // Ask player if they'd like to play again
     var playAgainConfirm = window.confirm("Would you like to play again?");
